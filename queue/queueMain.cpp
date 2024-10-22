69cb091c9060dc89f9dfedb9afd520d4a073abac
Epoza <86025026+Epoza@users.noreply.github.com>
2024-10-21 22:29:54 -0400

    add queue example

---
+++ queue/queueMain.cpp
@@ -0,0 +1,23 @@
+#include "queue.h"
+
+using namespace std;
+
+int main()
+{
+
+  class Queue q;
+  q.deQueue();
+
+  q.enQueue(1);
+  q.enQueue(2);
+  q.enQueue(3);
+  q.enQueue(4);
+  q.enQueue(5);
+  q.enQueue(6);
+
+  q.display();
+
+  q.deQueue();
+
+  q.display();
+}
